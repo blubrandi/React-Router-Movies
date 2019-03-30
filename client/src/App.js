a45ff8fba37ba56bf5a35457277e53dc2b9035b7
blubrandi <blubrandi0@gmail.com>
2019-03-30 11:10:10 -0600

    Linked movie card and set url to be dynamic movie id

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path='/' component={MovieList} />
-        <Route path='movies-list/:id' component={Movie}/>
+        <Route path='/movies/:id' component={Movie}/>
       </div>
     );
   }
